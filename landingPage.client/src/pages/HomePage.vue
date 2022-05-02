1f9feb4b8e860029b52d37b20eb3a2619d663032
Haven Fricke <robslaven@live.com>
2022-05-02 11:09:45 -0600

    styling push

--- landingPage.client/src/pages/HomePage.vue
+++ landingPage.client/src/pages/HomePage.vue
@@ -47,22 +47,20 @@
         class="col-5 rounded-bottom p-5 mt-5 text-primary mainfont bg-dark"
       >
         <h1 id="#" class="border-bottom tshadow textglow fs-2">ABOUT</h1>
-        <p class="text-white tshadow">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
-          quasi. Accusamus perspiciatis eligendi et similique consequuntur! Rem,
-          eius sint saepe voluptatem dolor delectus natus error aut blanditiis
-          quis quae omnis quia aliquam harum similique quod distinctio
-          asperiores. Tempora perspiciatis non dignissimos accusantium! Qui,
-          animi facilis sed alias culpa necessitatibus reiciendis consectetur,
-          eligendi facere accusamus accusantium cumque similique delectus libero
-          natus? Impedit, dolorem, debitis facilis maiores aliquam ratione
-          facere molestiae eos, nesciunt quisquam alias eligendi. Fugiat, quae
-          dolore voluptatum, tempora error maiores, tenetur doloremque
-          perferendis quasi quibusdam atque deserunt sit commodi molestias
-          labore officiis similique dolorum? Eligendi qui fugiat quis nobis
-          delectus commodi minima veniam, dolore iure, atque odio perferendis
-          earum exercitationem dolorem voluptatem vel pariatur nihil doloribus
-          unde.
+        <p class="text-white mt-2 tshadow">
+          Hello, My Name is Haven Fricke. I am a passionate and mission driven
+          software developer. With a keen eye for detail and design and a maze
+          of a mind for data engineering, I have tackled some big problems in
+          full stack software development. It is fun for me - software
+          development offers me an outlet for not only my scientific mind but
+          also my creative mind. The balance of the opposites is met in the code
+          and puts me directly in a state of flow. I believe in our future of
+          technology. I see it as a means of eveolving into something we never
+          imagined possible. The possibilities are endless in terms of what we
+          can do with it and deveolping the software to run our inventions is a
+          direct bridge between a gap of the individual using technology and the
+          technology itself. I firmly beleive in this and attempt to bringout
+          the principles it creates in my work.
         </p>
       </div>
 
@@ -82,18 +80,12 @@
         >
           <h1 class="mt-4 text-primary ms-2 textglow fs-2">More</h1>
           <p class="ms-3">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi
-            impedit nostrum vel excepturi maxime deserunt eius suscipit
-            exercitationem velit quam, esse cum debitis consequuntur aut nam
-            dolorem neque fugit numquam totam qui. Fuga quos dolor vitae
-            possimus officiis nostrum pariatur, aliquid laboriosam alias,
-            molestias magni optio deleniti? Nisi culpa est doloremque incidunt,
-            assumenda beatae maxime. Sed veritatis libero provident cumque esse
-            officiis totam? Porro blanditiis molestias nesciunt a sapiente
-            necessitatibus, officiis aut, quis incidunt in excepturi saepe,
-            numquam quos quam. Neque, obcaecati ut. Cupiditate dolorem iure
-            necessitatibus dolores eligendi ea quas nesciunt. Quos eos expedita
-            doloremque placeat dicta similique reiciendis?
+            Our future relies on innovators. I am one of those. I take extreme
+            care when it comes to resolving issues in the most efficient way
+            possible. Clean code and fast technologies are green solutions. The
+            excess consumption of power is at an all time high in our society so
+            it is extremely key to take the time to make the most efficient code
+            as possible. My individual effort makes an over all impact.
           </p>
         </div>
       </div>
@@ -135,122 +127,184 @@
         />
       </div>
     </div>
-    <div class="row mt-5 justify-content-start">
+    <div class="row mt-5 justify-content-around">
       <div
         style="z-index: 2"
-        class="col-6 rounded-bottom p-5 mt-5 text-primary mainfont"
+        class="col-5 rounded-bottom mt-5 p-5 text-primary mainfont"
       >
         <h1 class="tshadow fs-1 border-bottom textglow fs-2">SKILLS</h1>
 
         <div
           class="container-fluid rounded mt-3 d-flex justify-content-around row"
         >
+          <div class="row d-flex justify-content-around">
+            <img
+              id="this"
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              src="src\assets\img\vue.png"
+              alt=""
+              title="Vue Framework"
+            />
+            <img
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              id="that"
+              src="src\assets\img\javascript.png"
+              alt=""
+              title="JavaScript"
+            />
+            <img
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              id="andthis"
+              src="src\assets\img\mySql.png"
+              alt=""
+              title="MySQL Databases"
+            />
+          </div>
+
+          <div class="row d-flex justify-content-around">
+            <img
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              id="theother"
+              src="src\assets\img\node.png"
+              alt=""
+              title="Node.js and NPM Servers"
+            />
+            <img
+              class="img-fluid pt-4 col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              id="andthis"
+              src="src\assets\img\bootstrap-logo-vector.svg"
+              alt=""
+              title="Bootstrap 5 framework"
+            />
+            <img
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              id="this"
+              src="src\assets\img\csharp.png"
+              alt=""
+              title="C# language"
+            />
+          </div>
+
+          <div class="row d-flex justify-content-around">
+            <img
+              id="that"
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              src="src\assets\img\visual-studio-code.svg"
+              alt=""
+              title="Visual Studio Code"
+            />
+            <img
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              id="theother"
+              src="src\assets\img\HTML5_logo_and_wordmark.svg"
+              alt=""
+              title="HTML5"
+            />
+            <img
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              id="andthis"
+              src="src\assets\img\restapi.png"
+              alt=""
+              title="RESTful API conventions"
+            />
+          </div>
+          <div class="row d-flex justify-content-around">
+            <img
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              id="this"
+              src="src\assets\img\CSS3_logo_and_wordmark.svg"
+              alt=""
+              title="CSS"
+            />
+            <img
+              id="that"
+              class="img-fluid col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              src="src\assets\img\dot-NET-Core.png"
+              alt=""
+              title=".NET Framework"
+            />
+            <img
+              id="that"
+              class="img-fluid object-fit col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              src="src\assets\img\auth0.png"
+              alt=""
+              title="Auth0 Services"
+            />
+          </div>
+          <div class="row d-flex justify-content-around">
+            <img
+              id="theother"
+              class="img-fluid object-fit col-lg-2 col-md-6 col-sm-6 mt-5 mb-5"
+              src="src\assets\img\Git-Icon-1788C.png"
+              alt=""
+              title="Git"
+            />
+            <img
+              id="andthis"
+              class="
+                img-fluid
+                object-fit
+                px-0
+                col-lg-2 col-md-3 col-md-6
+                sm
+                mt-5
+                mb-5
+              "
+              src="src\assets\img\figma.png"
+              alt=""
+              title="Figma"
+            />
+            <img
+              id="this"
+              class="img-fluid object-fit col-lg-2 col-md-3 col-sm-6 mt-5 mb-5"
+              src="src\assets\img\scrum.png"
+              alt=""
+              title="Scrum Framework"
+            />
+          </div>
+        </div>
+      </div>
+      <div
+        style="z-index: 0"
+        class="
+          col-4
+          p-5
+          mx-1
+          text-white
+          rounded
+          bg-dark
+          mt-5
+          text-primary
+          mainfont
+        "
+      >
+        <div class="row d-flex justify-content-center">
           <img
-            id="this"
-            class="img-fluid col-2 mt-5 mb-5"
-            src="src\assets\img\vue.png"
+            class="rounded-circle img-fluid object-fit col-4 shadow"
+            src="src\assets\img\headshot pic me.png"
             alt=""
-            title="Vue Framework"
-          />
-          <img
-            class="img-fluid col-2 mt-5 mb-5"
-            id="that"
-            src="src\assets\img\javascript.png"
-            alt=""
-            title="JavaScript"
-          />
-          <img
-            class="img-fluid col-2 mt-5 mb-5"
-            id="andthis"
-            src="src\assets\img\mySql.png"
-            alt=""
-            title="MySQL Databases"
           />
 
-          <img
-            class="img-fluid col-2 mt-5 mb-5"
-            id="theother"
-            src="src\assets\img\node.png"
-            alt=""
-            title="Node.js and NPM Servers"
-          />
-          <img
-            class="img-fluid pt-4 col-2 mt-5 mb-5"
-            id="andthis"
-            src="src\assets\img\bootstrap-logo-vector.svg"
-            alt=""
-            title="Bootstrap 5 framework"
-          />
-          <img
-            class="img-fluid col-2 mt-5 mb-5"
-            id="this"
-            src="src\assets\img\csharp.png"
-            alt=""
-            title="C# language"
-          />
-          <img
-            id="that"
-            class="img-fluid col-2 mt-5 mb-5"
-            src="src\assets\img\visual-studio-code.svg"
-            alt=""
-            title="Visual Studio Code"
-          />
-          <img
-            class="img-fluid col-2 mt-5 mb-5"
-            id="theother"
-            src="src\assets\img\HTML5_logo_and_wordmark.svg"
-            alt=""
-            title="HTML5"
-          />
-          <img
-            class="img-fluid col-2 mt-5 mb-5"
-            id="andthis"
-            src="src\assets\img\restapi.png"
-            alt=""
-            title="RESTful API conventions"
-          />
-          <img
-            class="img-fluid col-2 mt-5 mb-5"
-            id="this"
-            src="src\assets\img\CSS3_logo_and_wordmark.svg"
-            alt=""
-            title="CSS"
-          />
-          <img
-            id="that"
-            class="img-fluid col-2 mt-5 mb-5"
-            src="src\assets\img\dot-NET-Core.png"
-            alt=""
-            title=".NET Framework"
-          />
-          <img
-            id="that"
-            class="img-fluid object-fit col-2 mt-5 mb-5"
-            src="src\assets\img\auth0.png"
-            alt=""
-            title="Auth0 Services"
-          />
-          <img
-            id="theother"
-            class="img-fluid object-fit col-2 mt-5 mb-5"
-            src="src\assets\img\Git-Icon-1788C.png"
-            alt=""
-            title="Git"
-          />
-          <img
-            id="andthis"
-            class="img-fluid object-fit px-0 col-2 mt-5 mb-5"
-            src="src\assets\img\figma.png"
-            alt=""
-            title="Figma"
-          />
-          <img
-            id="this"
-            class="img-fluid object-fit col-2 mt-5 mb-5"
-            src="src\assets\img\scrum.png"
-            alt=""
-            title="Scrum Framework"
-          />
+          <p class="col-8 text-center fs-1">HAVEN FRICKE</p>
+          <ul class="col-12 ms-5">
+            <div class="row d-flex justify-content-center">
+              <li class="my-3 col-12">Vue</li>
+              <li class="my-3 col-12">JavaScript</li>
+              <li class="my-3 col-12">MySQL</li>
+              <li class="my-3 col-12">Node.js</li>
+              <li class="my-3 col-12">Bootstrap</li>
+              <li class="my-3 col-12">C#</li>
+              <li class="my-3 col-12">Visual Studio Code</li>
+              <li class="my-3 col-12">HTML5</li>
+              <li class="my-3 col-12">REST API Conventions</li>
+              <li class="my-3 col-12">CSS & SCSS</li>
+              <li class="my-3 col-12">.NET Framework</li>
+              <li class="my-3 col-12">Auth0</li>
+              <li class="my-3 col-12">Git & GitHub</li>
+              <li class="my-3 col-12">Figma</li>
+              <li class="my-3 col-12">Scrum Framework</li>
+            </div>
+          </ul>
         </div>
       </div>
     </div>
@@ -262,8 +316,20 @@
       />
       <div class="row justify-content-end">
         <i class="mdi text-end text-primary fs-1 mdi-alert"></i>
-        <h6 class="border textglow border-primary p-1 col-3 mb-5 text-primary">
-          passion and mission driven developer
+        <h6
+          class="
+            border
+            textglow
+            border-primary
+            textglow
+            mainfont
+            p-1
+            col-3
+            mb-5
+            text-primary
+          "
+        >
+          keeping it clean keeps it green
           <span class="col-1 textinput mainfont mx-1 px-1"> </span>
         </h6>
       </div>
